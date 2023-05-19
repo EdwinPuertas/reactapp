093a0ca14d4465f381c0056039664f3b8f781843
Edwin Puertas <eapuerta@gmail.com>
2023-05-19 15:11:53 -0500

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+import { DataGrid } from '@mui/x-data-grid';
+
+
+function App() {
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+        setLoading(true)
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(json => setUsers(json))
+            .finally(() => {
+                setLoading(false)
+            })
+    }, [])
+
+    return (
+        <div className="App">
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <>
+                    <h1>Users</h1>
+                    <table border={1} align={"center"}>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                        </tr>
+                        {users.map(user => (
+                            <tr key={user.id}>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.phone}</td>
+                            </tr>
+                        ))}
+                    </table>
+                </>
+            )}
+        </div>
+    )
+}
+
+export default App
